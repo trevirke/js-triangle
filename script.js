fef853f0eb268cf477c6f1ab57caebc58863253b
Sylwia Mamczur <sylwia.mamczur@gmail.com>
2017-11-24 11:35:41 +0100

    Add files

---
+++ script.js
@@ -0,0 +1,10 @@
+//js Ćwiczenia
+
+var a = 10;
+var h = 4;
+var triangleArea = a*h/2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+var name = alert ('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
